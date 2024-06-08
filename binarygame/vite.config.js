fd5dba3e520bec77c340397195e12e9cf6f8a623
hanglma <martin.hangl@jhg-traunreut.de>
2024-06-08 12:06:03 +0200

    fix: game

--- binarygame/vite.config.js
+++ binarygame/vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/binarygame/', // Add this line
+  base: '', // Add this line
   // other config options...
   build: {
     outDir: 'build' // Change this to your preferred output directory
